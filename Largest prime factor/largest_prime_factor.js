cc7d48175302d815230a564380eaeacb7b66a241
Rafer45 <ataque992@hotmail.com>
2016-03-04 19:20:27 -0600

    Change code entirely; optimize

--- Largest prime factor/largest_prime_factor.js
+++ Largest prime factor/largest_prime_factor.js
@@ -1,58 +1,24 @@
-var number = 600851475143;
+var problemN = 600851475143;
 
-// Maybe not the most efficient way, but pretty easy to digest.
-// I'll havetoimprove it sometime later.
+function getHighestPrimeFactor(n) {
+    var i = 2,
+        myN = n;
 
- // Returns the 'not modulus result' operation that returns true if it's divisible.
-Number.prototype.isDivisibleBy = function(divisor) {
-    return !(this % divisor);
-}
+    // Since i always divides when possible, it
+    // exhausts all possible divisors before itself.
 
-// Returns whether or not a number is prime. Not efficient.
-Number.prototype.isPrime = function() {
-    for (var i = 2; i <= this/2; i++) {
-        if (this .isDivisibleBy(i)) {
-            return false;
-        }
-    }
-    return i >= 2;
-}
+    // If i itself squared is greater than n, n cannot
+    // be decomposed further, since the only possible
+    // remaining divisors are i and n.
 
-// Returns an array with n prime numbers, ascending from 2.
-function getNPrimes(n) {
-    var numList = [],
-        i = 2;
-    while(numList.length < n){
-        if (i .isPrime()) {
-            numList.push(i);
+    while (i * i < myN) {
+        while(myN % i == 0) {
+            myN /= i;
         }
         i++;
     }
-
-    return numList;
+    return myN;
 }
 
-// returns the nth prime number, starting from 2.
-function getNthPrime(n) {
-    return getNPrimes(n)[n-1];
-}
-
-// Returns an array with number's prime factors in ascending order.
-function decompose(n) {
-    var i = 1,
-        myN = n,
-        componentArr = [],
-        iPrime = getNthPrime(i);
-
-    while (myN != 1) {
-        if (myN .isDivisibleBy(iPrime)) {
-            myN /= iPrime;
-            componentArr.push(iPrime)
-        } else {
-            i++;
-            iPrime = getNthPrime(i);
-        }
-    }
-
-    return componentArr;
-}
+console.log(getHighestPrimeFactor(problemN))
+console.log(getHighestPrimeFactor(3141592653589))
